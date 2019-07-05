68bf723d792cc11656094f21f899fca11d3dd240
Morpheus Hsieh <morpheus.hsieh@gmail.com>
2019-07-06 00:00:53 +0800

    commit

---
+++ Java/src/main/_401_to_450/_443_StringCompression/Main443.java
@@ -0,0 +1,36 @@
+package main._401_to_450._443_StringCompression;
+
+public class Main443
+{
+  private TestCase443 _TestCase = new TestCase443();
+  
+  public static void main(String[] args)
+  {
+    System.out.println("443 Strign Compression");
+    
+    Solution443 sol = new Solution443v1();
+
+  }
+  
+  public static void init()
+  {
+  }
+  
+  public static void run()
+  {
+    
+  }
+
+}
+
+//["a","a","b","b","c","c","c"]
+//["a","2","b","2","c","3"]
+//6    
+//
+//["a"]
+//["a"]
+//1
+//  
+//["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+//["a","b","1","2"]
+//4
---
+++ Java/src/main/_401_to_450/_443_StringCompression/Solution443v1.java
@@ -0,0 +1,9 @@
+package main._401_to_450._443_StringCompression;
+
+public class Solution443v1 implements Solution443
+{
+  public int compress(char[] chars)
+  {
+    return -1;
+  }
+}
